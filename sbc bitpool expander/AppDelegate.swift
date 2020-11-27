aecb3e592d0a9dab6f944e145a3723d98b19120d
Alexander A. Kropotin <ololx@icloud.com>
2020-11-27 11:27:40 +0500

    chore(*) rewrite app on a swift

--- sbc bitpool expander/AppDelegate.swift
+++ sbc bitpool expander/AppDelegate.swift
@@ -2,16 +2,13 @@
 //  AppDelegate.swift
 //  sbc bitpool expander
 //
-//  Created by Alexander A. Kropotin on 26.11.2020.
+//  Created by Alexander A. Kropotin on 27.11.2020.
 //
 
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
