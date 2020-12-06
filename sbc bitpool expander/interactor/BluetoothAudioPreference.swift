245cc2b8302a1b9c8f7fbdba4fe32684c68541b7
Alexander A. Kropotin <ololx@icloud.com>
2020-12-06 17:00:08 +0500

    feature(BluetoothAudioPreference.swift) define Bluetooth Audio Preference protocol for storing blutoothaudiod setings

---
+++ sbc bitpool expander/interactor/BluetoothAudioPreference.swift
@@ -0,0 +1,15 @@
+//
+//  BluetoothAudioPreference.swift
+//  sbc bitpool expander
+//
+//  Created by Alexander A. Kropotin on 06.12.2020.
+//
+
+import Cocoa
+
+public protocol BluetoothAudioPreference {
+    
+    func save();
+    
+    func delete();
+}
